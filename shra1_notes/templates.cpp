20236d27c023f291c68719a4c0df3d3b5750c0f1
shra114 <shravan.ashannagari@gmail.com>
2020-08-29 20:43:02 +0530

    add templates notes

---
+++ shra1_notes/templates.cpp
@@ -0,0 +1,13 @@
+//templates
+Ex four fns to add - double and int combi
+
+generalization of functions
+template <typename T> 
+T add( T var1, T var2)
+{
+	return var1+var2;
+	}
+
+
+//class templates
+
