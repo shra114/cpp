766e70843378b730ad654246139414961e76b350
shra114 <shravan.ashannagari@gmail.com>
2020-08-29 19:35:33 +0530

    initial files

---
+++ projects/1_BankingSystem/shravan/notes.v
@@ -0,0 +1,24 @@
+//Account 
+  1. properties
+  	a. name
+	b. number
+	c. balance
+	d. active
+	e. type
+	f. statement
+  2. operations
+  	a. activate
+	b. deactivate
+	c. deposit -- only if active. is transfer?
+	d. withdraw -- only if active . is transfer?
+//Bank
+  1. Properties
+  	a. name
+	b. branch
+	c. accounts
+  2. Operations
+  	a. create account
+	b. delete account
+	c. transactions, deposit, withdraw, transfer
+	d. generate statement
+
